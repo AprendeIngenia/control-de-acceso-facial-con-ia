6448378a1282a7c42c67af9a188e947c57f84689
Santiago Sanchez Rios <85022752+AprendeIngenia@users.noreply.github.com>
2024-06-06 18:10:40 -0500

    Update facial_login.py

--- process/face_processing/facial_login.py
+++ process/face_processing/facial_login.py
@@ -1,33 +1,30 @@
 import numpy as np
-from typing import Any
-
-from numpy import ndarray
+import cv2
+from typing import Tuple
 
 from process.face_processing.face_utils import FaceUtils
 from process.database.config import DataBasePaths
 
 
-class FacialLogIn:
+class FaceLogIn:
     def __init__(self):
         self.face_utilities = FaceUtils()
         self.database = DataBasePaths()
 
-        self.face_comparison = False
         self.matcher = None
+        self.comparison = False
+        self.cont_frame = 0
 
-    def process(self, face_image: np.ndarray) -> tuple[ndarray, None, str] | tuple[ndarray, bool, str] | tuple[
-        ndarray, bool | Any, str]:
+    def process(self, face_image: np.ndarray):
         # step 1: check face detection
         check_face_detect, face_info, face_save = self.face_utilities.check_face(face_image)
         if check_face_detect is False:
-            self.face_comparison = False
-            self.matcher = None
-            return face_image, self.matcher, '¡No face detect!'
+            return face_image, self.matcher, '¡No face detected!'
 
         # step 2: face mesh
         check_face_mesh, face_mesh_info = self.face_utilities.face_mesh(face_image)
         if check_face_mesh is False:
-            return face_image, self.matcher, '¡No face mesh detect!'
+            return face_image, self.matcher, '¡No face mesh detected!'
 
         # step 3: extract face mesh
         face_mesh_points_list = self.face_utilities.extract_face_mesh(face_image, face_mesh_info)
@@ -39,32 +36,33 @@ class FacialLogIn:
         self.face_utilities.show_state_login(face_image, state=self.matcher)
 
         if check_face_center:
-            # step 5: extract face info
-            # bbox & key points
-            face_bbox = self.face_utilities.extract_face_bbox(face_image, face_info)
-            face_points = self.face_utilities.extract_face_points(face_image, face_info)
+            # step 6: extract face info
+            # bbox & key_points
+            self.cont_frame = self.cont_frame + 1
+            if self.cont_frame == 48:
+                face_bbox = self.face_utilities.extract_face_bbox(face_image, face_info)
+                face_points = self.face_utilities.extract_face_points(face_image, face_info)
 
-            # step 6: face aligned
-            face_aligned = self.face_utilities.face_alignment(face_save, face_points)
+                # step 7: face crop
+                face_crop = self.face_utilities.face_crop(face_save, face_bbox)
 
-            # step 7: face crop
-            face_crop = self.face_utilities.face_crop(face_aligned, face_bbox)
+                # step 8: read database
+                faces_database, names_database, info = self.face_utilities.read_face_database(self.database.faces)
 
-            # step 8: read face database
-            faces_database, names_database, info = self.face_utilities.read_face_database(self.database.faces)
+                if len(faces_database) != 0 and not self.comparison and self.matcher is None:
+                    self.comparison = True
+                    # step 9: compare faces
+                    self.matcher, user_name = self.face_utilities.face_matching(face_crop, faces_database, names_database)
 
-            if len(faces_database) != 0 and not self.face_comparison and self.matcher is None:
-                self.face_comparison = True
-                # step 9: compare faces
-                self.matcher, user_name = self.face_utilities.face_matching(face_crop, faces_database, names_database)
-
-                if self.matcher:
-                    # step 10: save date & time
-                    self.face_utilities.user_register(user_name, self.database.users)
-                    return face_image, self.matcher, 'Approved user access!'
+                    if self.matcher:
+                        # step 10: save data & time
+                        self.face_utilities.user_check_in(user_name, self.database.users)
+                        return face_image, self.matcher, 'Approved user access!'
+                    else:
+                        return face_image, self.matcher, 'User no approved'
                 else:
-                    return face_image, self.matcher, 'User no approved!'
+                    return face_image, self.matcher, 'Empty database'
             else:
-                return face_image, self.matcher, 'Empty database!'
-
-        return face_image, self.matcher, 'No center face!'
+                return face_image, self.matcher, 'wait frames'
+        else:
+            return face_image, self.matcher, 'No center face'
